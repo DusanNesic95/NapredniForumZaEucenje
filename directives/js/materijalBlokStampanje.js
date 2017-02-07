05057f88b9d6c1361edd7048ab7bbfb02c35f1d0
DusanNesic95 <dusannesic001@gmail.com>
2017-02-07 01:11:03 +0100

    Obrisan front-end sa repozitorijuma za back-end, ubacen greskom.

--- directives/js/materijalBlokStampanje.js
+++
@@ -1,9 +0,0 @@
-app.directive('materijalBlokStampanje', function(funkcije) { 
-  return { 
-    restrict: 'E', 
-    scope: { 
-      materijal: '=' 
-    }, 
-    templateUrl: '../directives/templates/materijalBlokStampanje.html'
-  }; 
-});
